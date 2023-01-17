4c285f861b72139f4aff24554ee31cab6eac25be
masprog2022 <masprog2022@gmail.com>
2023-01-17 02:37:39 +0100

    :sparkles: feat: testing product controller

--- backend/src/main/java/com/masprog/api/products/controllers/ProductController.java
+++ backend/src/main/java/com/masprog/api/products/controllers/ProductController.java
@@ -1,5 +1,14 @@
 package com.masprog.api.products.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+	
+	@GetMapping("/")
+	public String route() {
+		return "Api funcionando bem!";
+	}
 
 }
