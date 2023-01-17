9b3e9539bc8aee0d471097692200372522d5b8b7
masprog2022 <masprog2022@gmail.com>
2023-01-17 02:33:18 +0100

    :sparkles: feat: add product repository

--- backend/src/main/java/com/masprog/api/products/repositories/ProductRepository.java
+++ backend/src/main/java/com/masprog/api/products/repositories/ProductRepository.java
@@ -1,5 +1,11 @@
 package com.masprog.api.products.repositories;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.masprog.api.products.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
