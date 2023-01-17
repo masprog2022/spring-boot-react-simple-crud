9ae3d13b1668fdba094d770eb4ed93ebf1b86b10
masprog2022 <masprog2022@gmail.com>
2023-01-17 02:20:59 +0100

    :tada: Project created

---
+++ backend/src/test/java/com/masprog/api/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masprog.api.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
