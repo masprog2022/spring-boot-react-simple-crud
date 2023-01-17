dcca89407047a6af50c86c917d7277e20e3d692e
masprog2022 <masprog2022@gmail.com>
2023-01-17 02:46:47 +0100

    :sparkles: feat: add method findAll()

--- backend/src/main/java/com/masprog/api/products/services/ProductService.java
+++ backend/src/main/java/com/masprog/api/products/services/ProductService.java
@@ -1,5 +1,20 @@
 package com.masprog.api.products.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.masprog.api.products.entities.Product;
+import com.masprog.api.products.repositories.ProductRepository;
+
+@Service
 public class ProductService {
 
+	@Autowired
+	private ProductRepository repository;
+	
+	public Iterable<Product> findAll(){
+		
+		return repository.findAll();
+	}
+	
 }
