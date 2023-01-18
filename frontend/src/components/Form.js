fcd5e0111cf3266dcc97beb81270ba1de4101e03
masprog2022 <masprog2022@gmail.com>
2023-01-18 14:06:37 +0100

    :sparkles: feat: add estructure form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Form() {
   return (
     <div>
-         <h1>Hello Form</h1>
+         <form>
+            <input type='text' placeholder='Name' />
+            <input type='text' placeholder='brand' />
+
+            <input type='button' value='Register' />
+            <input type='button' value='Update' />
+            <input type='button' value='Remove' />
+            <input type='button' value='Cancel' />
+         </form>
     </div>
   )
 }
