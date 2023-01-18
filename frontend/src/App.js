89a15116811e238f627e91be265c44e4d8a04795
masprog2022 <masprog2022@gmail.com>
2023-01-18 16:32:34 +0100

    :sparkles: feat: get products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
-import { useState  } from "react";
+import { useState, useEffect  } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Table from "./components/Table";
 
 function App() {
 
+  // UseState
   const [btnRegister, setBtnRegister] = useState(true);
+  const [product, setProduct] = useState([])
 
+  // UseEffect
+
+  useEffect(() => {
+    fetch("http://localhost:8080/products")
+    .then(data => data.json())
+    .then(data_inverted => setProduct(data_inverted))
+  
+  }, [])
+
+  // Return
   return (
     <div className="App">
+      <p>{JSON.stringify(product)}</p>
       <Form button={btnRegister} />
       <Table />
     </div>
